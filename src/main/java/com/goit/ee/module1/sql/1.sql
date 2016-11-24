4ff2b4b22388816d84f073c9d2a4a78acb4c7122
usonictw <usonictw@bigmir.net>
2016-11-24 19:24:24 +0200

    added column 'salary'  to  tables developers

---
+++ src/main/java/com/goit/ee/module1/sql/1.sql
@@ -0,0 +1,8 @@
+ALTER TABLE developers ADD COLUMN salary INT;
+UPDATE developers SET  salary = 2000 WHERE id = 1;
+UPDATE developers SET  salary = 1200 WHERE id = 2;
+UPDATE developers SET  salary = 2500 WHERE id = 3;
+UPDATE developers SET  salary = 2200 WHERE id = 4;
+UPDATE developers SET  salary = 2700 WHERE id = 5;
+UPDATE developers SET  salary = 8000 WHERE id = 6;
+UPDATE developers SET  salary = 1300 WHERE id = 7;
