7b8e450141061da8d2f41b554383bed939de4491
usonictw <usonictw@bigmir.net>
2016-11-24 14:18:29 +0200

    add populateDB.sql

--- src/main/java/com/goit/ee/module1/sql/populateDB.sql
+++ src/main/java/com/goit/ee/module1/sql/populateDB.sql
@@ -1,6 +1,39 @@
-CREATE TABLE dev_skills (id SERIAL NOT NULL,
- id_dev INT NOT NULL, 
- id_skills INT NOT NULL, 
- CONSTRAINT "dev_scills_pk" PRIMARY KEY (id), 
- CONSTRAINT devs_devs_skills FOREIGN KEY (id_dev) REFERENCES developers (id), 
- CONSTRAINT devs_skills_skills FOREIGN KEY (id_skills) REFERENCES skills (id));
+INSERT INTO developers (id, first_name, last_name, age) VALUES (DEFAULT, 'Ivan', 'Ivanov', 23);
+INSERT INTO developers (id, first_name, last_name, age) VALUES (DEFAULT, 'Sergey', 'Seergeev', 27);
+INSERT INTO developers (id, first_name, last_name, age) VALUES (DEFAULT, 'Петр', 'Петров', 29);
+INSERT INTO developers (id, first_name, last_name, age) VALUES (DEFAULT,'John', 'Johnson', 32);
+INSERT INTO developers (id, first_name, last_name, age) VALUES (DEFAULT, 'Kate', 'Johanson', 34);
+INSERT INTO developers (id, first_name, last_name, age) VALUES (DEFAULT, 'Ju', 'Yong', 32);
+INSERT INTO developers (id, first_name, last_name, age) VALUES (DEFAULT, 'Michel', 'Svetlov', 43);
+
+INSERT INTO skills (id, name) VALUES (DEFAULT, 'C#');
+INSERT INTO skills (id, name) VALUES (DEFAULT, 'C++');
+INSERT INTO skills (id, name) VALUES (DEFAULT, 'Java');
+INSERT INTO skills (id, name) VALUES (DEFAULT, 'PHP');
+INSERT INTO skills (id, name) VALUES (DEFAULT, 'JavaScript');
+INSERT INTO skills (id, name) VALUES (DEFAULT, 'Ruby');
+
+INSERT INTO companies (id, name, address) VALUES (DEFAULT, 'GlobalSoft', 'Kiev');
+INSERT INTO companies (id, name, address) VALUES (DEFAULT, 'U-Soft', 'Poland');
+INSERT INTO companies (id, name, address) VALUES (DEFAULT, 'Ukr-Soft', 'Kiev');
+INSERT INTO companies (id, name, address) VALUES (DEFAULT, 'NetSoftAO', 'Dnepr');
+INSERT INTO companies (id, name, address) VALUES (DEFAULT, 'HiSoft', 'Kiev');
+
+INSERT INTO customers (id, name, address) VALUES (DEFAULT, 'LLC "E-EQ-NT"', 'Kiev');
+INSERT INTO customers (id, name, address) VALUES (DEFAULT, 'KyivgorAdmin', 'Kiev');
+INSERT INTO customers (id, name, address) VALUES (DEFAULT, 'Ukrainian Railway', 'Kiev');
+INSERT INTO customers (id, name, address) VALUES (DEFAULT, '24Film', 'Kiev');
+
+INSERT INTO projects (id, name) VALUES (DEFAULT, 'e-store');
+INSERT INTO projects (id, name) VALUES (DEFAULT, 'Kinonline');
+INSERT INTO projects (id, name) VALUES (DEFAULT, 'on-line-gov');
+INSERT INTO projects (id, name) VALUES (DEFAULT, 'uz-booking');
+INSERT INTO projects (id, name) VALUES (DEFAULT, 'NetApplication#1');
+INSERT INTO projects (id, name) VALUES (DEFAULT, 'NetApplication#2');
+INSERT INTO projects (id, name) VALUES (DEFAULT, 'WebApplication#3');
+
+
+
+
+
+
