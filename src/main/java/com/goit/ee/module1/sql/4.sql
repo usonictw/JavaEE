43caf93fa09a9ef463b850bc912a470107f4d62e
usonictw <usonictw@bigmir.net>
2016-11-24 19:30:25 +0200

    added column cost to table projects

---
+++ src/main/java/com/goit/ee/module1/sql/4.sql
@@ -0,0 +1,8 @@
+ALTER TABLE projects ADD COLUMN cost INT;
+UPDATE projects SET  cost = 20000 WHERE id = 1;
+UPDATE projects SET  cost = 11200 WHERE id = 2;
+UPDATE projects SET  cost = 10500 WHERE id = 3;
+UPDATE projects SET  cost = 22800 WHERE id = 4;
+UPDATE projects SET  cost = 80000 WHERE id = 5;
+UPDATE projects SET  cost = 6000 WHERE id = 6;
+UPDATE projects SET  cost = 13300 WHERE id = 7;
