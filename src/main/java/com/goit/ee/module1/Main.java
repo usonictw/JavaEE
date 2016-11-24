1879f4e09e8c97d8a392db50a2cdf7b54c167d8a
javatier <tierr@ukr.net>
2016-11-24 10:05:53 +0200

    add maven

--- src/main/java/com/goit/ee/module1/Main.java
+++ src/main/java/com/goit/ee/module1/Main.java
@@ -2,6 +2,6 @@ package com.goit.ee.module1;
 
 public class Main{
     public static void main(String[] args) {
-        System.out.println("SQL fuck");
+        System.out.println("SQL mfk");
     }
 }
