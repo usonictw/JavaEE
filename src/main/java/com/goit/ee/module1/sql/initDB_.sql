7691799c2470db5007245a7beb1445e04bdd7dd3
usonictw <usonictw@bigmir.net>
2016-11-25 22:39:26 +0200

    test

--- src/main/java/com/goit/ee/module1/sql/initDB_.sql
+++ src/main/java/com/goit/ee/module1/sql/initDB_.sql
@@ -1,9 +1,34 @@
 -- noinspection SqlDialectInspectionForFile
-CREATE TABLE developers (id SERIAL PRIMARY KEY NOT NULL, first_name VARCHAR(15) NOT NULL, last_name VARCHAR(20) NOT NULL, age INT NOT NULL);
-CREATE TABLE skills (id SERIAL PRIMARY KEY NOT NULL, name VARCHAR(15) NOT NULL);
-CREATE TABLE projects (id SERIAL PRIMARY KEY NOT NULL, name VARCHAR(50) NOT NULL);
-CREATE TABLE companies (id SERIAL PRIMARY KEY NOT NULL, name VARCHAR(30) NOT NULL, address VARCHAR(50));
-CREATE TABLE customers (id SERIAL PRIMARY KEY NOT NULL, name VARCHAR(30) NOT NULL, address VARCHAR(50));
-CREATE TABLE dev_skills (id SERIAL PRIMARY KEY NOT NULL, id_dev INT, id_skills INT);
+CREATE TABLE developers (
+  id SERIAL PRIMARY KEY NOT NULL,
+  first_name VARCHAR(15) NOT NULL,
+  last_name VARCHAR(20) NOT NULL,
+  age INT NOT NULL);
+
+CREATE TABLE skills (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(15) NOT NULL);
+
+CREATE TABLE projects (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(50) NOT NULL);
+
+CREATE TABLE companies (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  address VARCHAR(50));
+
+CREATE TABLE customers (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  address VARCHAR(50));
+
+CREATE TABLE dev_skills (
+  id        SERIAL PRIMARY KEY NOT NULL,
+  id_dev    INT REFERENCES developers (id),
+  id_skills INT,
+  CONSTRAINT dev_skills___fk
+  FOREIGN KEY (id_dev),
+);
 CREATE TABLE proj_devs (id SERIAL PRIMARY KEY NOT NULL, id_proj INT, id_dev INT);
 CREATE TABLE comp_projs (id SERIAL PRIMARY KEY NOT NULL, id_comp INT, id_projs INT);
