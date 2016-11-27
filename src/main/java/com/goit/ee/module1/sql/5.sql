eba7006b0f476afd7c0fefc5e4915615af0c7d84
usonictw <usonictw@bigmir.net>
2016-11-27 14:13:07 +0200

    addaed file 5.sql

---
+++ src/main/java/com/goit/ee/module1/sql/5.sql
@@ -0,0 +1,10 @@
+SELECT customers.name AS cust_name,
+      companies.name AS comp_name,
+      projects.cost,
+      projects.name AS proj_name
+FROM projects
+  JOIN cust_proj ON projects.id = cust_proj.id_proj
+  JOIN customers ON cust_proj.id_cust = customers.id
+  JOIN comp_proj ON projects.id = comp_proj.id_proj
+  JOIN companies ON comp_proj.id_comp = companies.id
+ORDER BY cost LIMIT 1;
