ca4e6e985367db24b007172091635923ee907463
Alexandr Baranov <heydrich@ukr.net>
2016-12-04 09:59:55 +0200

    DAO interface

--- src/main/java/com/goit/ee/module2/dao/DeveloperDAOImpl.java
+++ src/main/java/com/goit/ee/module2/dao/DeveloperDAOImpl.java
@@ -1,7 +1,32 @@
 package com.goit.ee.module2.dao;
 
+import com.goit.ee.module2.dto.Developer;
+
 /**
  * Created by Alex on 04.12.2016.
  */
-public class DeveloperDAOImpl {
+public class DeveloperDAOImpl implements DevelopersDAO {
+    public boolean create(Developer developer) {
+        return false;
+    }
+
+    public boolean get(long id) {
+        return false;
+    }
+
+    public boolean update(Developer developer) {
+        return false;
+    }
+
+    public boolean delete(long id) {
+        return false;
+    }
+
+    public boolean findByName(String name) {
+        return false;
+    }
+
+    public boolean findByNameAndLastName(String name, String lastName) {
+        return false;
+    }
 }
