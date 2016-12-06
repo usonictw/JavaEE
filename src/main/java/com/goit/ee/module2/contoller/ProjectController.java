722b7beacd7c45f0cb0b557e37fad05c8acb4627
usonictw <0937354341us>
2016-12-06 14:17:15 +0200

    done ProjectController

--- src/main/java/com/goit/ee/module2/contoller/ProjectController.java
+++ src/main/java/com/goit/ee/module2/contoller/ProjectController.java
@@ -2,55 +2,80 @@ package com.goit.ee.module2.contoller;
 
 import com.goit.ee.module2.dao.ProjectDAOImpl;
 import com.goit.ee.module2.dto.Project;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * Created by user on 05.12.2016.
- */
 public class ProjectController implements Executable {
 
-    private String dataInput() {
-        String dataInput = null;
-        int countLine = 1;
-        while (countLine > 0) {
-            try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-                dataInput = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            countLine--;
-        }
-
-        return dataInput;
-    }
+    private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private ProjectDAOImpl projectDAO = new ProjectDAOImpl();
 
     @Override
     public void execute(Command command) {
-        ProjectDAOImpl projectDAO = new ProjectDAOImpl();
-        if (command.equals(Command.CREATE)) {
-            System.out.println("Input projects name");
-            String projectName = dataInput();
-            System.out.println("Input projects cost");
-            String projCostStr = dataInput();
-            int projectCost = 0;
-            try {
-                projectCost = Integer.valueOf(projCostStr);
-            } catch (NumberFormatException e) {
-                System.out.println("input cost correctly");
-            }
-            projectDAO.create(new Project(1, projectName, projectCost));
+
+        switch (command) {
+            case CREATE:
+                Project project = new Project();
+                project.setName(getProjectName());
+                project.setCost(getProjectCost());
+                projectDAO.create(project);
+            case READ:
+                projectDAO.get(getProjectId());
+            case UPDATE:
+                projectDAO.update(new Project(getProjectId(), getProjectName(), getProjectCost()));
+            case DELETE:
+                projectDAO.delete(getProjectId());
         }
     }
 
+    private String getProjectName() {
+        String projectName = null;
+        System.out.println("Input projects name");
+        try {
+            projectName = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return projectName;
+    }
 
-    public static void main(String[] args) {
+    private int getProjectCost() {
+        String projCostStr = null;
+        int projectCost = 0;
+        System.out.println("Input projects cost");
+        try {
+            projCostStr = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (projCostStr != null) {
+                projectCost = Integer.valueOf(projCostStr);
+            }
+        } catch (NumberFormatException | NullPointerException e) {
+            System.out.println("input cost correctly");
+        }
+        return projectCost;
+    }
 
-        ProjectController projectController = new ProjectController();
-
-        projectController.execute(Command.CREATE);
+    private int getProjectId() {
+        String projIdStr = null;
+        int projectId = 0;
+        System.out.println("Input projects id");
+        try {
+            projIdStr = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (projIdStr != null) {
+                projectId = Integer.valueOf(projIdStr);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("input cost correctly");
+        }
+        return projectId;
     }
 }
 
